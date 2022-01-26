9d96b91e7682b02b37e85e044ac0541cc124e902
sti <sti@matera.com>
2022-01-25 23:21:39 -0300

    Finalização do capítulo de Stream API

--- IntelliJ/src/streams/Aluno.java
+++ IntelliJ/src/streams/Aluno.java
@@ -1,5 +1,7 @@
 package streams;
 
+import java.util.Objects;
+
 public class Aluno {
 
 	final String nome;
@@ -15,6 +17,24 @@ public class Aluno {
 		this.nota = nota;
 		this.bomComportamento = bomComportamento;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return  "nome='" + nome + '\'' +
+				", nota=" + nota +
+				", bomComportamento=" + bomComportamento;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Aluno aluno = (Aluno) o;
+		return Double.compare(aluno.nota, nota) == 0 && bomComportamento == aluno.bomComportamento && Objects.equals(nome, aluno.nome);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome, nota, bomComportamento);
+	}
 }
